2ec473e710379c06dfe6b224946d1d2cae884bb7
Mizuki_Tanisawa <wasaizumi@gmail.com>
2020-05-23 15:56:32 +0900

    指示待ち君リトライ

---
+++ EL4S_01_team4/Assets/Script/Game/Player/Character.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+	// パターン列挙
+	public enum Pattern
+	{
+		Follow,
+		Ignore
+	}
+
+	// リストインデックス
+	private int m_current_pattern = 0;
+
+	// パターンリスト
+	[SerializeField]
+	private List<Pattern> m_pattern_list;
+
+	public bool IsFollow()
+	{
+		// インデックス更新前の番号
+		int _current = m_current_pattern;
+
+		// インデックス更新
+		m_current_pattern = (m_current_pattern + 1) % m_pattern_list.Count;
+
+		return m_pattern_list[_current] == Pattern.Follow;
+	}
+}
---
+++ EL4S_01_team4/Assets/Script/Game/Player/Instruction.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instruction : MonoBehaviour
+{
+    /// <summary>
+    /// 左を押されたらtrue違ったらfalse
+    /// </summary>
+    private bool _pushLeftIsEnble;
+
+    /// <summary>
+    /// 左を押されたらtrue違ったらfalseを返す
+    /// </summary>
+    public bool PushLeftIsEnable { get { return _pushLeftIsEnble; } }
+
+    private bool isPush = false;
+
+    /// <summary>
+    /// 押された瞬間にtrueを返す
+    /// </summary>
+    public bool IsPush { get{ return isPush; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPush = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isPush = false;
+    }
+
+    public void PushLeft()
+    {
+        Debug.Log("スタンリーそこを左に曲がるんだ");
+        _pushLeftIsEnble = true;
+        isPush = true;
+    }
+
+    public void PushRight()
+    {
+        Debug.Log("スタンリーそこを右にに曲がるんだ");
+        _pushLeftIsEnble = false;
+        isPush = true;
+    }
+}
